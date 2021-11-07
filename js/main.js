3ca7ece0ea7494854f517725c0a353566df07343
Artem <Artem.godfather@yandex.ru>
2021-11-07 13:55:57 +0500

    adaptive,

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// Мобильная версия
+
+const mobileNavButton = document.querySelector('.mobile-nav-button');
+const mobileNavIcon = document.querySelector('.mobile-nav-button__icon');
+const mobileNav = document.querySelector('.mobile-nav');
+
+mobileNavButton.addEventListener('click', function () {
+	mobileNavIcon.classList.toggle('active');
+	mobileNav.classList.toggle('active');
+	document.body.classList.toggle('no-scroll');
+});
+
+
+// Video
+
+
+const videoBtn = document.querySelector('#video-story-btn');
+const videoBtnIcon = document.querySelector('#video-story-btn-icon');
+const videoFile = document.querySelector('#video-story');
+const videoOverlay = document.querySelector('#video-story-overlay');
+
+
+videoBtn.addEventListener('click', function() {
+  
+
+
+
+  // Зарефакторить код 
+  if(videoFile.paused) {
+    videoFile.play();
+    videoBtnIcon.src = "./img/story/pause-white.svg"
+    
+    
+  }
+  else {
+    videoFile.pause();
+    videoBtnIcon.src = "./img/story/play-white.svg"
+    
+  }
+
+
+
+
+})
